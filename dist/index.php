bd46377d1ff708daf16795a9679cb9e158683965
yogiueo <120150087+yogiueo@users.noreply.github.com>
2023-08-03 14:32:49 +0700

    login

--- dist/index.php
+++ dist/index.php
@@ -1,17 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="lgn.css" rel="stylesheet">
+	<title>Membuat Login Dengan PHP dan MySQLi - WWW.MALASNGODING.COM</title>
 </head>
 <body>
-    <div class="bck">
-        <img src="img/gambar.png"/>
-        <div class="card">
-    
-        </div>
-    </div>
+	<h2>Login - www.malasngoding.com</h2>
+	<br/>
+	<!-- cek pesan notifikasi -->
+	<?php 
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan'] == "gagal"){
+			echo "Login gagal! username dan password salah!";
+		}else if($_GET['pesan'] == "logout"){
+			echo "Anda telah berhasil logout";
+		}else if($_GET['pesan'] == "belum_login"){
+			echo "Anda harus login untuk mengakses halaman admin";
+		}
+	}
+	?>
+	<br/>
+	<br/>
+	<form method="post" action="cek_login.php">
+		<table>
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input type="text" name="username" placeholder="Masukkan username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input type="password" name="password" placeholder="Masukkan password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td><input type="submit" value="LOGIN"></td>
+			</tr>
+		</table>			
+	</form>
 </body>
 </html>
