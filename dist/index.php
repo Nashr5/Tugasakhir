6b93e035440a056955c54fa37a2887c804adec65
yogiueo <120150087+yogiueo@users.noreply.github.com>
2023-08-03 15:13:09 +0700

    memperbaiki login

--- dist/index.php
+++ dist/index.php
@@ -1,43 +1,36 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Membuat Login Dengan PHP dan MySQLi - WWW.MALASNGODING.COM</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="lgn.css" rel="stylesheet">
 </head>
 <body>
-	<h2>Login - www.malasngoding.com</h2>
-	<br/>
-	<!-- cek pesan notifikasi -->
-	<?php 
-	if(isset($_GET['pesan'])){
-		if($_GET['pesan'] == "gagal"){
-			echo "Login gagal! username dan password salah!";
-		}else if($_GET['pesan'] == "logout"){
-			echo "Anda telah berhasil logout";
-		}else if($_GET['pesan'] == "belum_login"){
-			echo "Anda harus login untuk mengakses halaman admin";
-		}
-	}
-	?>
-	<br/>
-	<br/>
-	<form method="post" action="cek_login.php">
-		<table>
-			<tr>
-				<td>Username</td>
-				<td>:</td>
-				<td><input type="text" name="username" placeholder="Masukkan username"></td>
-			</tr>
-			<tr>
-				<td>Password</td>
-				<td>:</td>
-				<td><input type="password" name="password" placeholder="Masukkan password"></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td></td>
-				<td><input type="submit" value="LOGIN"></td>
-			</tr>
-		</table>			
-	</form>
+<div style="width: 100%; height: 100%; position: relative; background: linear-gradient(180deg, #F9F9F9 0%, rgba(255, 57.37, 57.37, 0.79) 44%, rgba(0, 0, 0, 0.73) 96%)">
+    <div style="width: 1366px; height: 768px; left: 0px; top: 0px; position: absolute; background: rgba(0, 0, 0, 0.36)"></div>
+    <img style="width: 333px; height: 150px; left: 534px; top: 0px; position: absolute" src="img/logo.png" />
+    <div style="width: 444px; height: 374px; left: 479px; top: 209px; position: absolute">
+        <div style="width: 444px; height: 374px; left: 0px; top: 0px; position: absolute; background: rgba(0, 0, 0, 0.39); border-radius: 40px; border: 1.50px #A14F4F solid; backdrop-filter: blur(25px)"></div>
+        <div style="width: 128px; height: 29px; left: 173px; top: 39px; position: absolute; color: #FFF6F6; font-size: 40px; font-family: Bayon; font-weight: 400; word-wrap: break-word">Login</div>
+        <div style="width: 321.74px; height: 31.59px; left: 63.70px; top: 263.50px; position: absolute; background: #AF0E0E; border-radius: 10px"></div>
+        <div style="width: 66px; height: 24px; left: 192px; top: 267px; position: absolute; color: #FFF6F6; font-size: 20px; font-family: Bayon; font-weight: 400; word-wrap: break-word">Sign in</div>
+        <div style="width: 321.74px; height: 115px; left: 61.77px; top: 102.37px; position: absolute">
+            <div style="width: 321.74px; height: 31.59px; left: 0px; top: 16.43px; position: absolute; background: #FFF6F6; border-radius: 10px; border: 0.50px #BCBEC0 solid"></div>
+            <div style="width: 28.31px; height: 13.27px; left: 0px; top: 0px; position: absolute; color: #FFF6F6; font-size: 18px; font-family: Gilroy; font-weight: 400; word-wrap: break-word">Email</div>
+            <div style="width: 321.74px; height: 31.59px; left: 0px; top: 83.41px; position: absolute; background: #FFF6F6; border-radius: 10px; border: 0.50px #BCBEC0 solid"></div>
+            <div style="width: 50.19px; height: 13.27px; left: 0px; top: 66.98px; position: absolute; color: #FFF6F6; font-size: 18px; font-family: Gilroy; font-weight: 400; word-wrap: break-word">Password</div>
+            <div style="width: 158px; height: 28px; left: 14.23px; top: 16.63px; position: absolute; color: #838383; font-size: 14px; font-family: Sansation Light; font-weight: 300; word-wrap: break-word">username@gmail.com</div>
+            <div style="width: 10.30px; height: 10.11px; left: 297.93px; top: 94.15px; position: absolute; border-radius: 40px; overflow: hidden">
+                <div style="width: 2.64px; height: 2.60px; left: 4.69px; top: 3.13px; position: absolute; background: #C7D2D6"></div>
+                <div style="width: 6.11px; height: 4.76px; left: 3.78px; top: 2.11px; position: absolute; background: #C7D2D6"></div>
+                <div style="width: 8.64px; height: 7.72px; left: 0.68px; top: 1.20px; position: absolute; background: #C7D2D6"></div>
+            </div>
+        </div>
+    </div>
+    <div style="left: 555px; top: 401px; position: absolute; color: #838383; font-size: 14px; font-family: Sansation Light; font-weight: 300; word-wrap: break-word">password</div>
+</div>     
+    
 </body>
 </html>
